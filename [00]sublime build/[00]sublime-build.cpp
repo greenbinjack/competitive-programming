dcd37012ba6d0e5b3b6748297f850dd527fa94ad
Saom Bin Khaled <greenbinjack@gmail.com>
2026-05-28 13:27:29 +0600

    update

--- [00]sublime build/[00]sublime-build.cpp
+++ [00]sublime build/[00]sublime-build.cpp
@@ -1,12 +1,20 @@
-#for linux
+# for linux
 {
   "shell_cmd": "g++ $file -o $file_base_name && ./$file_base_name<input.txt> output.txt && rm $file_base_name",
   "working_dir": "$file_path",
   "selector": "source.c++"
 }
-#for windows
+# for windows
 {
-  "shell_cmd": "g++ -std=c++17 $file -o $file_base_name.exe && $file_base_name.exe <input.txt> output.txt && del $file_base_name.exe",
-  "working_dir":"$file_path",
-  "selector":"source.c++"
+  "shell_cmd": "ccache g++ -std=c++23 -Wall -Wextra -DLOCAL \"$file\" -o \"$file_base_name.exe\" && \"$file_base_name.exe\" < input.txt > output.txt",
+  "working_dir": "$file_path",
+  "selector": "source.c++",
+  "shell": true
 }
+# for interactive - windows
+{
+    "shell_cmd": "g++ -std=c++23 \"$file\" -o \"$file_base_name.exe\" && start cmd /k \"$file_base_name.exe\"",
+    "working_dir": "$file_path",
+    "selector": "source.c++"
+}
+
